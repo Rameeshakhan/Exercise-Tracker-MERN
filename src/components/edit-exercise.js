6447952b0af1affb1642c0e9e6842e3788cd1c56
Rameesha Khan <rameeshakhan75@gmail.com>
2023-01-22 23:25:58 +0500

    Front End First commit

---
+++ src/components/edit-exercise.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditExercise extends Component {
+    render() {
+        return (
+        <div>
+            <p>you are on the edit exercise list component</p>
+        </div>
+        ) 
+    }
+}
